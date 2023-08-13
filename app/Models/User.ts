7cd8622ab99dba743895936ede0348168cd63700
Han Wang <xboxhc3@gmail.com>
2023-08-13 14:08:59 +0800

    add user table and model

---
+++ app/Models/User.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public name: string
+
+  @column.date()
+  public verifiedAt: DateTime
+
+  @column()
+  public googleId: string
+
+  @column()
+  public facebookId: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
---
+++ database/migrations/1691548658567_users.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('email').unique().notNullable().comment('username')
+      table.string('password').notNullable()
+      table.string('name')
+      table.timestamp('verifiedAt', { useTz: true }).notNullable()
+      table.string('googleId')
+      table.string('facebookId')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
